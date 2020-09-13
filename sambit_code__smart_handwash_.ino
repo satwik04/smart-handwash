d9a4521fd1c2394da9c806b5be1957cb4a0835a3
sambit221 <56729866+sambit221@users.noreply.github.com>
2020-09-13 21:13:40 +0530

    arduino project
    Arduino code for smart handwash.

---
+++ sambit_code__smart_handwash_.ino
@@ -0,0 +1,38 @@
+#include <Servo.h>   
+Servo servo;     
+int trigPin = 5;    
+int echoPin = 6;   
+int servoPin = 7;
+long duration, dist;    
+
+
+void setup() {       
+    Serial.begin(9600);
+    servo.attach(7);  
+    pinMode(trigPin, OUTPUT);  
+    pinMode(echoPin, INPUT); 
+       servo.write(0);  
+   
+} 
+
+void loop() {  
+digitalWrite(trigPin, LOW);
+delayMicroseconds(10);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+duration = pulseIn(echoPin, HIGH);
+dist = (duration/2) / 29.1;    //to obtain distance as speed of sound is 343m/s that is (in cm/microseconds) equal to 1/29.1
+
+ if ( dist<10 ) {
+ Serial.println("the distance is less than 10");
+ servo.write(90);  
+ delay(1000);
+ }     
+else{
+  servo.write(0);
+}
+     
+Serial.print(dist);
+}
